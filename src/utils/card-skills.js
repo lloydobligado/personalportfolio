43568ca23bb160e39aee5187d42768e2195e8223
Lloyd Carie M. Obligado <lloydobligado@gmail.com>
2023-08-09 22:32:25 +0800

    fixes: card skills id

--- src/utils/card-skills.js
+++ src/utils/card-skills.js
@@ -15,72 +15,72 @@ const TechSkills = [
         name: "JavaScript"
     },
     {
-        id: "2023LCO03",
+        id: "2023LCO04",
         icons: "./src/assets/images/skills/typescript.svg",
         name: "TypeScript"
     },
     {
-        id: "2023LCO07",
+        id: "2023LCO05",
         icons: "./src/assets/images/skills/jquery.png",
         name: "JQuery"
     },
     {
-        id: "2023LCMO11",
+        id: "2023LCMO06",
         icons: "./src/assets/images/skills/sass.png",
         name: "SASS"
     },
     {
-        id: "2023LCMO12",
+        id: "2023LCMO07",
         icons: "./src/assets/images/skills/bootstrap.png",
         name: "Bootstrap"
     },
     {
-        id: "2023LCMO13",
+        id: "2023LCMO08",
         icons: "./src/assets/images/skills/nodejs.png",
         name: "NodeJS"
     },
     {
-        id: "2023LCO08",
+        id: "2023LCO09",
         icons: "./src/assets/images/skills/npm.png",
         name: "NPM"
     },
     {
-        id: "2023LCO28",
+        id: "2023LCO10",
         icons: "./src/assets/images/skills/git.png",
         name: "Git"
     },
     {
-        id: "2023LCO29",
+        id: "2023LCO11",
         icons: "./src/assets/images/skills/github.png",
         name: "Github"
     },
     {
-        id: "2023LCMO14",
+        id: "2023LCMO12",
         icons: "./src/assets/images/skills/reactjs.png",
         name: "ReactJS"
     },
     {
-        id: "2023LCMO15",
+        id: "2023LCMO13",
         icons: "./src/assets/images/skills/vuejs.png",
         name: "VueJS"
     },
     {
-        id: "2023LCMO16",
+        id: "2023LCMO14",
         icons: "./src/assets/images/skills/vuetify.png",
         name: "Vuetify"
     },
     {
-        id: "2023LCMO17",
+        id: "2023LCMO15",
         icons: "./src/assets/images/skills/tailwindcss.png",
         name: "TailwindCSS"
     },
     {
-        id: "2023LCMO17",
+        id: "2023LCMO16",
         icons: "./src/assets/images/skills/mui.png",
         name: "Material UI"
     },
     {
-        id: "2023LCO04",
+        id: "2023LCMO17",
         icons: "./src/assets/images/skills/php.png",
         name: "PHP"
     },
@@ -90,67 +90,67 @@ const TechSkills = [
         name: "CodeIgniter"
     },
     {
-        id: "2023LCMO21",
+        id: "2023LCMO19",
         icons: "./src/assets/images/skills/mssql.png",
         name: "SQL Server"
     },
     {
-        id: "2023LCMO22",
+        id: "2023LCMO20",
         icons: "./src/assets/images/skills/mysql.png",
         name: "MySQL"
     },
     // {
-    //     id: "2023LCO05",
+    //     id: "2023LCMO21",
     //     icons: "./src/assets/images/skills/html.png",
     //     name: "JAVA"
     // },
     {
-        id: "2023LCO06",
+        id: "2023LCMO22",
         icons: "./src/assets/images/skills/python.png",
         name: "Python"
     },
     // {
-    //     id: "2023LCO09",
+    //     id: "2023LCMO23",
     //     icons: "./src/assets/images/skills/html.png",
     //     name: "JWT"
     // },
     {
-        id: "2023LCO10",
+        id: "2023LCMO24",
         icons: "./src/assets/images/skills/.net.png",
         name: "VB.NET"
     },
     // {
-    //     id: "2023LCMO19",
+    //     id: "2023LCMO25",
     //     icons: "./src/assets/images/skills/html.png",
     //     name: "Apache"
     // },
     // {
-    //     id: "2023LCMO20",
+    //     id: "2023LCMO26",
     //     icons: "./src/assets/images/skills/html.png",
     //     name: "MariaDB"
     // },
     {
-        id: "2023LCMO23",
+        id: "2023LCMO27",
         icons: "./src/assets/images/skills/arduino.png",
         name: "Arduino"
     },
     {
-        id: "2023LCMO24",
+        id: "2023LCMO28",
         icons: "./src/assets/images/skills/figma.png",
         name: "Figma"
     },
     {
-        id: "2023LCMO25",
+        id: "2023LCMO29",
         icons: "./src/assets/images/skills/canva.png",
         name: "Canva"
     },
     {
-        id: "2023LCMO26",
+        id: "2023LCMO30",
         icons: "./src/assets/images/skills/vercel.png",
         name: "Vercel"
     },
     {
-        id: "2023LCMO27",
+        id: "2023LCMO31",
         icons: "./src/assets/images/skills/netlify.png",
         name: "Netlify"
     },
